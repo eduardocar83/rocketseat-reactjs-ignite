949997b29b5ab4419ecc352e9701d8f1fb2546ef
eduardocar83 <eduardocar83@gmail.com>
2022-02-28 20:41:23 -0300

    01-github-explorer Estado do Componente

--- 01-github-explorer/src/App.jsx
+++ 01-github-explorer/src/App.jsx
@@ -1,8 +1,14 @@
 import './styles/global.scss';
 import React from 'react';
 import { RepositoryList } from './components/RepositoryList';
+import { Counter } from './components/Counter';
 
 
 export function App() {
-    return <RepositoryList />
+    return (
+        <>
+            <RepositoryList />
+            <Counter />
+        </>
+    )
 }
